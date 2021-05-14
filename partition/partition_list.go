0108aea17fcd157be176c5d78745356ae92d5618
nakabonne <ryo@nakao.dev>
2021-05-14 14:02:52 +0900

    Add linked list for partitions

---
+++ partition/partition_list.go
@@ -0,0 +1,242 @@
+package partition
+
+import (
+	"fmt"
+	"sync"
+	"sync/atomic"
+)
+
+// PartitionList represents a linked list for partitions.
+// Each partition is arranged in order order of newest to oldest.
+// That is, the head node is always the newest, the tail node is the oldest.
+// Only head partition should be writable.
+//
+// FYI: Partitions are frequently added/deleted, on the other hand,
+// no need to take values by specifying indexes. That's why linked list is suitable.
+type PartitionList interface {
+	// Insert appends a new node to the head.
+	Insert(partition Partition)
+	// Remove eliminates the given partition from the list.
+	Remove(partition Partition) error
+	// Swap replaces the old partition with the new one.
+	Swap(old, new Partition) error
+	// GetHead gives back the head node which is the newest one.
+	GetHead() Partition
+	// Size returns the size of itself.
+	Size() int
+	// NewIterator gives back the iterator object fot this list.
+	// If you need to inspect all nodes within the list, use this one.
+	NewIterator() Iterator
+}
+
+// Iterator represents an iterator for partition list. The basic usage is:
+/*
+  for iterator.Next() {
+    partition, err := iterator.Value()
+    // Do something with partition
+  }
+*/
+type Iterator interface {
+	// Next positions the iterator at the next node in the list.
+	// It will be positioned at the head on the first call.
+	// The return value will be true if a value can be read from the list.
+	Next() bool
+	// Value gives back the current Partition in the iterator.
+	Value() (Partition, error)
+
+	currentNode() *partitionNode
+}
+
+type partitionList struct {
+	size int64
+	head *partitionNode
+	tail *partitionNode
+	mu   sync.RWMutex
+}
+
+func NewPartitionList() PartitionList {
+	return &partitionList{}
+}
+
+func (p *partitionList) GetHead() Partition {
+	p.mu.RLock()
+	defer p.mu.RUnlock()
+	return p.head.value()
+}
+
+func (p *partitionList) Insert(partition Partition) {
+	node := &partitionNode{
+		val: partition,
+	}
+	p.mu.RLock()
+	head := p.head
+	p.mu.RUnlock()
+	if head != nil {
+		node.next = head
+	}
+
+	p.setHead(node)
+	atomic.AddInt64(&p.size, 1)
+}
+
+func (p *partitionList) Remove(target Partition) error {
+	if p.Size() <= 0 {
+		return fmt.Errorf("empty partition")
+	}
+
+	// Iterate over itself from the head.
+	var prev, next *partitionNode
+	iterator := p.NewIterator()
+	for iterator.Next() {
+		current := iterator.currentNode()
+		if !samePartitions(current.value(), target) {
+			prev = current
+			continue
+		}
+
+		// Remove the current node.
+
+		iterator.Next()
+		next = iterator.currentNode()
+		switch {
+		case prev == nil:
+			// removing the head node
+			p.setHead(next)
+		case next == nil:
+			// removing the tail node
+			prev.setNext(nil)
+			p.setTail(prev)
+		default:
+			// removing the middle node
+			prev.setNext(next)
+		}
+		atomic.AddInt64(&p.size, -1)
+		return nil
+	}
+
+	return fmt.Errorf("the given partition was not found")
+}
+
+func (p *partitionList) Swap(old, new Partition) error {
+	if p.Size() <= 0 {
+		return fmt.Errorf("empty partition")
+	}
+
+	// Iterate over itself from the head.
+	var prev, next *partitionNode
+	iterator := p.NewIterator()
+	for iterator.Next() {
+		current := iterator.currentNode()
+		if !samePartitions(current.value(), old) {
+			prev = current
+			continue
+		}
+
+		// Swap the current node.
+
+		newNode := &partitionNode{
+			val:  new,
+			next: current.next,
+		}
+		iterator.Next()
+		next = iterator.currentNode()
+		switch {
+		case prev == nil:
+			// swapping the head node
+			p.setHead(newNode)
+		case next == nil:
+			// swapping the tail node
+			prev.setNext(newNode)
+			p.setTail(newNode)
+		default:
+			// swapping the middle node
+			prev.setNext(newNode)
+		}
+		return nil
+	}
+
+	return fmt.Errorf("the given partition was not found")
+}
+
+func samePartitions(x, y Partition) bool {
+	// TODO: Use ULID for identifier of partition
+	return x.MinTimestamp() == y.MinTimestamp()
+}
+
+func (p *partitionList) Size() int {
+	return int(atomic.LoadInt64(&p.size))
+}
+
+func (p *partitionList) NewIterator() Iterator {
+	p.mu.RLock()
+	head := p.head
+	p.mu.RUnlock()
+	// Put a dummy node so that it positions the head on the first Next() call.
+	dummy := &partitionNode{
+		next: head,
+	}
+	return &iterator{
+		current: dummy,
+	}
+}
+
+func (p *partitionList) setHead(node *partitionNode) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	p.head = node
+}
+
+func (p *partitionList) setTail(node *partitionNode) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	p.tail = node
+}
+
+// partitionNode wraps a Partition to hold the pointer to the next one.
+type partitionNode struct {
+	// val is immutable
+	val  Partition
+	next *partitionNode
+	mu   sync.RWMutex
+}
+
+// value gives back the actual Partition of the node.
+func (p *partitionNode) value() Partition {
+	return p.val
+}
+
+func (p *partitionNode) setNext(node *partitionNode) {
+	p.mu.Lock()
+	defer p.mu.Unlock()
+	p.next = node
+}
+
+func (p *partitionNode) getNext() *partitionNode {
+	p.mu.RLock()
+	defer p.mu.RUnlock()
+	return p.next
+}
+
+type iterator struct {
+	current *partitionNode
+}
+
+func (i *iterator) Next() bool {
+	if i.current == nil {
+		return false
+	}
+	next := i.current.getNext()
+	i.current = next
+	return i.current != nil
+}
+
+func (i *iterator) Value() (Partition, error) {
+	if i.current == nil {
+		return nil, fmt.Errorf("partition not found")
+	}
+	return i.current.value(), nil
+}
+
+func (i *iterator) currentNode() *partitionNode {
+	return i.current
+}
---
+++ partition/partition_list_test.go
@@ -0,0 +1,427 @@
+package partition
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func Test_partitionList_Remove(t *testing.T) {
+	tests := []struct {
+		name              string
+		partitionList     partitionList
+		target            Partition
+		wantErr           bool
+		wantPartitionList partitionList
+	}{
+		{
+			name:          "empty partition",
+			partitionList: partitionList{},
+			wantErr:       true,
+		},
+		{
+			name: "remove the head node",
+			partitionList: func() partitionList {
+				second := &partitionNode{
+					val: &fakePartition{
+						minTimestamp: 2,
+					},
+				}
+
+				first := &partitionNode{
+					val: &fakePartition{
+						minTimestamp: 1,
+					},
+					next: second,
+				}
+				return partitionList{
+					size: 2,
+					head: first,
+					tail: second,
+				}
+			}(),
+			target: &fakePartition{
+				minTimestamp: 1,
+			},
+			wantPartitionList: partitionList{
+				size: 1,
+				head: &partitionNode{
+					val: &fakePartition{
+						minTimestamp: 2,
+					},
+				},
+				tail: &partitionNode{
+					val: &fakePartition{
+						minTimestamp: 2,
+					},
+				},
+			},
+		},
+		{
+			name: "remove the tail node",
+			partitionList: func() partitionList {
+				second := &partitionNode{
+					val: &fakePartition{
+						minTimestamp: 2,
+					},
+				}
+
+				first := &partitionNode{
+					val: &fakePartition{
+						minTimestamp: 1,
+					},
+					next: second,
+				}
+				return partitionList{
+					size: 2,
+					head: first,
+					tail: second,
+				}
+			}(),
+			target: &fakePartition{
+				minTimestamp: 2,
+			},
+			wantPartitionList: partitionList{
+				size: 1,
+				head: &partitionNode{
+					val: &fakePartition{
+						minTimestamp: 1,
+					},
+				},
+				tail: &partitionNode{
+					val: &fakePartition{
+						minTimestamp: 1,
+					},
+				},
+			},
+		},
+		{
+			name: "remove the middle node",
+			partitionList: func() partitionList {
+				third := &partitionNode{
+					val: &fakePartition{
+						minTimestamp: 3,
+					},
+				}
+				second := &partitionNode{
+					val: &fakePartition{
+						minTimestamp: 2,
+					},
+					next: third,
+				}
+				first := &partitionNode{
+					val: &fakePartition{
+						minTimestamp: 1,
+					},
+					next: second,
+				}
+				return partitionList{
+					size: 3,
+					head: first,
+					tail: third,
+				}
+			}(),
+			target: &fakePartition{
+				minTimestamp: 2,
+			},
+			wantPartitionList: partitionList{
+				size: 2,
+				head: &partitionNode{
+					val: &fakePartition{
+						minTimestamp: 1,
+					},
+					next: &partitionNode{
+						val: &fakePartition{
+							minTimestamp: 3,
+						},
+					},
+				},
+				tail: &partitionNode{
+					val: &fakePartition{
+						minTimestamp: 3,
+					},
+				},
+			},
+		},
+		{
+			name: "given node not found",
+			partitionList: func() partitionList {
+				second := &partitionNode{
+					val: &fakePartition{
+						minTimestamp: 2,
+					},
+				}
+
+				first := &partitionNode{
+					val: &fakePartition{
+						minTimestamp: 1,
+					},
+					next: second,
+				}
+				return partitionList{
+					size: 2,
+					head: first,
+					tail: second,
+				}
+			}(),
+			target: &fakePartition{
+				minTimestamp: 3,
+			},
+			wantPartitionList: func() partitionList {
+				second := &partitionNode{
+					val: &fakePartition{
+						minTimestamp: 2,
+					},
+				}
+
+				first := &partitionNode{
+					val: &fakePartition{
+						minTimestamp: 1,
+					},
+					next: second,
+				}
+				return partitionList{
+					size: 2,
+					head: first,
+					tail: second,
+				}
+			}(),
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.partitionList.Remove(tt.target)
+			assert.Equal(t, tt.wantErr, err != nil)
+			assert.Equal(t, tt.wantPartitionList, tt.partitionList)
+		})
+	}
+}
+
+func Test_partitionList_Swap(t *testing.T) {
+	tests := []struct {
+		name              string
+		partitionList     partitionList
+		old               Partition
+		new               Partition
+		wantErr           bool
+		wantPartitionList partitionList
+	}{
+		{
+			name:          "empty partition",
+			partitionList: partitionList{},
+			wantErr:       true,
+		},
+		{
+			name: "swap the head node",
+			partitionList: func() partitionList {
+				second := &partitionNode{
+					val: &fakePartition{
+						minTimestamp: 2,
+					},
+				}
+
+				first := &partitionNode{
+					val: &fakePartition{
+						minTimestamp: 1,
+					},
+					next: second,
+				}
+				return partitionList{
+					size: 2,
+					head: first,
+					tail: second,
+				}
+			}(),
+			old: &fakePartition{
+				minTimestamp: 1,
+			},
+			new: &fakePartition{
+				minTimestamp: 100,
+			},
+			wantPartitionList: partitionList{
+				size: 2,
+				head: &partitionNode{
+					val: &fakePartition{
+						minTimestamp: 100,
+					},
+					next: &partitionNode{
+						val: &fakePartition{
+							minTimestamp: 2,
+						},
+					},
+				},
+				tail: &partitionNode{
+					val: &fakePartition{
+						minTimestamp: 2,
+					},
+				},
+			},
+		},
+		{
+			name: "swap the tail node",
+			partitionList: func() partitionList {
+				second := &partitionNode{
+					val: &fakePartition{
+						minTimestamp: 2,
+					},
+				}
+
+				first := &partitionNode{
+					val: &fakePartition{
+						minTimestamp: 1,
+					},
+					next: second,
+				}
+				return partitionList{
+					size: 2,
+					head: first,
+					tail: second,
+				}
+			}(),
+			old: &fakePartition{
+				minTimestamp: 2,
+			},
+			new: &fakePartition{
+				minTimestamp: 100,
+			},
+			wantPartitionList: partitionList{
+				size: 2,
+				head: &partitionNode{
+					val: &fakePartition{
+						minTimestamp: 1,
+					},
+					next: &partitionNode{
+						val: &fakePartition{
+							minTimestamp: 100,
+						},
+					},
+				},
+				tail: &partitionNode{
+					val: &fakePartition{
+						minTimestamp: 100,
+					},
+				},
+			},
+		},
+		{
+			name: "swap the middle node",
+			partitionList: func() partitionList {
+				third := &partitionNode{
+					val: &fakePartition{
+						minTimestamp: 3,
+					},
+				}
+				second := &partitionNode{
+					val: &fakePartition{
+						minTimestamp: 2,
+					},
+					next: third,
+				}
+
+				first := &partitionNode{
+					val: &fakePartition{
+						minTimestamp: 1,
+					},
+					next: second,
+				}
+				return partitionList{
+					size: 3,
+					head: first,
+					tail: third,
+				}
+			}(),
+			old: &fakePartition{
+				minTimestamp: 2,
+			},
+			new: &fakePartition{
+				minTimestamp: 100,
+			},
+			wantPartitionList: partitionList{
+				size: 3,
+				head: &partitionNode{
+					val: &fakePartition{
+						minTimestamp: 1,
+					},
+					next: &partitionNode{
+						val: &fakePartition{
+							minTimestamp: 100,
+						},
+						next: &partitionNode{
+							val: &fakePartition{
+								minTimestamp: 3,
+							},
+						},
+					},
+				},
+				tail: &partitionNode{
+					val: &fakePartition{
+						minTimestamp: 3,
+					},
+				},
+			},
+		},
+		{
+			name: "given node not found",
+			partitionList: func() partitionList {
+				second := &partitionNode{
+					val: &fakePartition{
+						minTimestamp: 2,
+					},
+				}
+
+				first := &partitionNode{
+					val: &fakePartition{
+						minTimestamp: 1,
+					},
+					next: second,
+				}
+				return partitionList{
+					size: 2,
+					head: first,
+					tail: second,
+				}
+			}(),
+			old: &fakePartition{
+				minTimestamp: 100,
+			},
+			wantPartitionList: partitionList{
+				size: 2,
+				head: &partitionNode{
+					val: &fakePartition{
+						minTimestamp: 1,
+					},
+					next: &partitionNode{
+						val: &fakePartition{
+							minTimestamp: 2,
+						},
+					},
+				},
+				tail: &partitionNode{
+					val: &fakePartition{
+						minTimestamp: 2,
+					},
+				},
+			},
+			wantErr: true,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := tt.partitionList.Swap(tt.old, tt.new)
+			assert.Equal(t, tt.wantErr, err != nil)
+			assert.Equal(t, tt.wantPartitionList, tt.partitionList)
+			/*
+				// Check if the partition list is as same as we'd like
+				iterator := tt.partitionList.NewIterator()
+				partitons := make([]Partition, 0, tt.partitionList.Size())
+				for iterator.Next() {
+					v, err := iterator.Value()
+					assert.NoError(t, err)
+					partitons = append(partitons, v)
+				}
+				assert.Equal(t, tt.wantPartitions, partitons)
+			*/
+		})
+	}
+}
