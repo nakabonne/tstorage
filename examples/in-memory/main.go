8c4ff780fd729577c18de995b5eee50ea98efd5b
nakabonne <ryo@nakao.dev>
2021-05-16 16:53:52 +0900

    Update example to use labels

--- examples/in-memory/main.go
+++ examples/in-memory/main.go
@@ -7,8 +7,34 @@ import (
 )
 
 func main() {
-	_, err := tstorage.NewStorage()
+	storage, err := tstorage.NewStorage()
 	if err != nil {
 		log.Fatal(err)
+	}
+	labels := []tstorage.Label{
+		{
+			Name:  "__name__",
+			Value: "metric1",
+		},
+	}
+	err = storage.InsertRows([]tstorage.Row{
+		{
+			Labels: labels,
+			DataPoint: tstorage.DataPoint{
+				Timestamp: 1600000,
+				Value:     0.1,
+			},
+		},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	iterator, _, err := storage.SelectRows(labels, 1600000, 1600001)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for iterator.Next() {
+		log.Printf("value: %v\n", iterator.Value())
 	}
 }
