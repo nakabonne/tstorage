eb6f8f925c4b879da2c8b47168c460f82c8de574
nakabonne <ryo@nakao.dev>
2021-05-19 22:20:04 +0900

    Add benchmark test for three rows insertion

--- storage_benchmark_test.go
+++ storage_benchmark_test.go
@@ -7,8 +7,24 @@ func BenchmarkStorage_InsertRows(b *testing.B) {
 	if err != nil {
 		panic(err)
 	}
+	b.ResetTimer()
 	for i := 0; i < b.N; i++ {
 		storage.InsertRows([]Row{
+			{Metric: "metric1", DataPoint: DataPoint{Timestamp: int64(i), Value: 0.1}},
+		})
+	}
+}
+
+func BenchmarkStorage_InsertRows_three_rows(b *testing.B) {
+	storage, err := NewStorage()
+	if err != nil {
+		panic(err)
+	}
+	b.ResetTimer()
+	for i := 0; i < b.N; i++ {
+		storage.InsertRows([]Row{
+			{Metric: "metric1", DataPoint: DataPoint{Timestamp: int64(i), Value: 0.1}},
+			{Metric: "metric1", DataPoint: DataPoint{Timestamp: int64(i), Value: 0.1}},
 			{Metric: "metric1", DataPoint: DataPoint{Timestamp: int64(i), Value: 0.1}},
 		})
 	}
