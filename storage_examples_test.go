6bb21a8139427005b43e9099b3d8ccf97adba4da
nakabonne <ryo@nakao.dev>
2021-05-28 10:20:52 +0900

    Add test to simulate reading from disk

--- storage_examples_test.go
+++ storage_examples_test.go
@@ -3,6 +3,8 @@ package tstorage_test
 import (
 	"errors"
 	"fmt"
+	"io/ioutil"
+	"os"
 	"sync"
 	"time"
 
@@ -85,6 +87,96 @@ func ExampleStorage_InsertRows_SelectDataPoints_concurrent() {
 		}
 	}()
 	wg.Wait()
+}
+
+// simulates writing and reading on disk.
+func ExampleStorage_InsertRows_SelectDataPoints_on_disk() {
+	tmpDir, err := ioutil.TempDir("", "tstorage-example")
+	if err != nil {
+		panic(err)
+	}
+	defer os.RemoveAll(tmpDir)
+
+	storage, err := tstorage.NewStorage(
+		tstorage.WithDataPath(tmpDir),
+		tstorage.WithPartitionDuration(10*time.Second),
+		tstorage.WithTimestampPrecision(tstorage.Seconds),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	for timestamp := int64(1600000000); timestamp < 1600000050; timestamp++ {
+		err := storage.InsertRows([]tstorage.Row{
+			{Metric: "metric1", DataPoint: tstorage.DataPoint{Timestamp: timestamp}},
+		})
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	// Start read workers that read 100 times in concurrent, as fast as possible.
+	points, err := storage.SelectDataPoints("metric1", nil, 1600000000, 1600000050)
+	if errors.Is(err, tstorage.ErrNoDataPoints) {
+		return
+	}
+	if err != nil {
+		panic(err)
+	}
+	for _, p := range points {
+		fmt.Printf("timestamp: %v, value: %v\n", p.Timestamp, p.Value)
+	}
+	// Output:
+	//timestamp: 1600000000, value: 0
+	//timestamp: 1600000001, value: 0
+	//timestamp: 1600000002, value: 0
+	//timestamp: 1600000003, value: 0
+	//timestamp: 1600000004, value: 0
+	//timestamp: 1600000005, value: 0
+	//timestamp: 1600000006, value: 0
+	//timestamp: 1600000007, value: 0
+	//timestamp: 1600000008, value: 0
+	//timestamp: 1600000009, value: 0
+	//timestamp: 1600000010, value: 0
+	//timestamp: 1600000011, value: 0
+	//timestamp: 1600000012, value: 0
+	//timestamp: 1600000013, value: 0
+	//timestamp: 1600000014, value: 0
+	//timestamp: 1600000015, value: 0
+	//timestamp: 1600000016, value: 0
+	//timestamp: 1600000017, value: 0
+	//timestamp: 1600000018, value: 0
+	//timestamp: 1600000019, value: 0
+	//timestamp: 1600000020, value: 0
+	//timestamp: 1600000021, value: 0
+	//timestamp: 1600000022, value: 0
+	//timestamp: 1600000023, value: 0
+	//timestamp: 1600000024, value: 0
+	//timestamp: 1600000025, value: 0
+	//timestamp: 1600000026, value: 0
+	//timestamp: 1600000027, value: 0
+	//timestamp: 1600000028, value: 0
+	//timestamp: 1600000029, value: 0
+	//timestamp: 1600000030, value: 0
+	//timestamp: 1600000031, value: 0
+	//timestamp: 1600000032, value: 0
+	//timestamp: 1600000033, value: 0
+	//timestamp: 1600000034, value: 0
+	//timestamp: 1600000035, value: 0
+	//timestamp: 1600000036, value: 0
+	//timestamp: 1600000037, value: 0
+	//timestamp: 1600000038, value: 0
+	//timestamp: 1600000039, value: 0
+	//timestamp: 1600000040, value: 0
+	//timestamp: 1600000041, value: 0
+	//timestamp: 1600000042, value: 0
+	//timestamp: 1600000043, value: 0
+	//timestamp: 1600000044, value: 0
+	//timestamp: 1600000045, value: 0
+	//timestamp: 1600000046, value: 0
+	//timestamp: 1600000047, value: 0
+	//timestamp: 1600000048, value: 0
+	//timestamp: 1600000049, value: 0
 }
 
 /*
